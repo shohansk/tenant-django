2dd907b6ea53a42acee45230fe5f2e1285e946c7
unknown <shohankhansk111@gmail.com>
2022-09-02 20:41:12 +0600

    tenant

---
+++ sweet_tenant/models.py
@@ -0,0 +1,11 @@
+from unicodedata import name
+from django.db import models
+from sweet_shared.models import SweetType
+# Create your models here.
+class Sweet(models.Model):
+    sweet_type = models.ForeignKey(SweetType,on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+    
